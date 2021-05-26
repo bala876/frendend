ce0e86e8496b06f09f9d55f5f6049775c4b301dd
bala876 <milky016918@gmail.com>
2021-05-26 12:33:54 +0530

    Updates

---
+++ static/js/main.e5004309.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmylife=this.webpackJsonpmylife||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);e(1);var i=e(3),o=e.n(i);e(8),e.p,e(9),e(0);var c=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,i=t.getFID,o=t.getFCP,c=t.getLCP,u=t.getTTFB;e(n),i(n),o(n),c(n),u(n)}))};o.a.render(document.getElementById("root")),c()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.e5004309.chunk.js.map
